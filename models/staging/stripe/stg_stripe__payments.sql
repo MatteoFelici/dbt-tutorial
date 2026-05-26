5771204a5c53e7a2d5fd5b0f0b5d38495f8f262a
Matteo Felici <matteofelici87@gmail.com>
2026-05-26 13:37:11 +0000

    added source file for Jaffle Shop

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,5 +1,8 @@
 select
-    id AS payment_id,
-    orderid AS order_id,
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    created as payment_date,
     amount
 from dbt-tutorial.stripe.payment
