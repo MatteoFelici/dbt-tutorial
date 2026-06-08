cb3985c77d72c8813d1560e222c2a6aab8585bca
Matteo Felici <matteofelici87@gmail.com>
2026-06-08 10:47:14 +0000

    add cents_to_dollars macro + added to int_orders__pivoted

--- models/intermediate/int_orders__pivoted.sql
+++ models/intermediate/int_orders__pivoted.sql
@@ -8,7 +8,7 @@ with payments as (
         order_id,
         {% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
         {% for method in payment_methods %}
-            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount
+            sum(case when payment_method = '{{ method }}' then {{ cents_to_dollars("amount") }} else 0 end) as {{ method }}_amount
             {%- if not loop.last -%}, {% endif %}
         {% endfor %}
     from payments
