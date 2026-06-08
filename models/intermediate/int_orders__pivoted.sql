c776646f1aab5a6782e997279a3d01fd244095e2
Matteo Felici <matteofelici87@gmail.com>
2026-06-08 09:33:20 +0000

    add int_orders__pivoted

---
+++ models/intermediate/int_orders__pivoted.sql
@@ -0,0 +1,18 @@
+with payments as (
+    select *
+    from {{ ref("stg_stripe__payments") }}
+    where payment_status = 'success'
+)
+, pivoted as (
+    select
+        order_id,
+        {% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
+        {% for method in payment_methods %}
+            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount
+            {%- if not loop.last -%}, {% endif %}
+        {% endfor %}
+    from payments
+    group by 1
+)
+select *
+from pivoted
